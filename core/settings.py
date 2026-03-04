3e9b3d200d96bcc24b352fbce124211fb18abb54
Siena Lia <macarena.p.r@outlook.es>
2026-03-04 01:48:58 -0300

    Disable media settings in core/settings.py
    Comment out MEDIA_URL and MEDIA_ROOT settings

--- core/settings.py
+++ core/settings.py
@@ -128,8 +128,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
